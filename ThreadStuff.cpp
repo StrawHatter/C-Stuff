2cfa8810b9b9f7199be7679debc5f86541c095b1
Paul Dreczkowski <StrawHatter@users.noreply.github.com>
2017-06-12 19:45:45 +0100

    Create ThreadStuff.cpp

---
+++ ThreadStuff.cpp
@@ -0,0 +1,49 @@
+# include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <thread> 
+
+using std::thread;
+using std::iostream;
+using namespace std;
+
+
+//defining struct integers for pointer things
+struct threadArgs { 
+	int foo;
+	int bar;
+};
+
+//void thread that takes in pointer struct *ptr
+//outputs the values before changing it, and returning it
+void myThreadFunc(threadArgs *ptr) {
+
+	cout << (*ptr).foo << endl;
+	cout << (*ptr).bar << endl;
+	ptr->foo = 7;
+
+
+}
+
+//main function sets a pointer variable *ptr and normal var args
+//of type threadArgs
+//the address of variable args is stored in the pointer variable ptr
+//then the member of args is accessed by a pointer 
+//point values 
+//make a new thread that exeucted the myThreadFunc with the ptr variable args
+//once thread finishes, join it and output the changed value. 
+int main() {
+	
+	threadArgs *ptr, args;
+
+	ptr = &args;
+
+	ptr->foo = 5;
+	ptr->bar = 2;
+
+	thread myThread(myThreadFunc, ptr);
+	myThread.join();
+	cout << (*ptr).foo << endl;
+}
+
+
